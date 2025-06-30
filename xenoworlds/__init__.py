0c23cb969f904c0a82315d4feebcfa110c19a351
RandallBalestriero <rbalestriero@meta.com>
2025-06-30 22:01:52 +0000

    set state

--- xenoworlds/__init__.py
+++ xenoworlds/__init__.py
@@ -5,3 +5,4 @@ from .world import World
 from .agent import Agent
 from .wm import DummyWorldModel
 from . import wrappers
+from .utils import set_state
--- xenoworlds/utils.py
+++ xenoworlds/utils.py
@@ -1,21 +1,63 @@
+from .world import World
 
 
-def render_states():
+def set_state(world: World, state):
+    for i in range(world.num_envs):
+        if hasattr(world.envs.envs[i].unwrapped, "state"):
+            world.envs.envs[i].unwrapped.state = state[i]
+        elif hasattr(world.envs.envs[i].unwrapped, "s"):
+            world.envs.envs[i].unwrapped.s = state[i]
+        elif hasattr(world.envs.envs[i].unwrapped.sim, "set_state_from_flattened"):
+            world.envs.envs[i].unwrapped.sim.set_state_from_flattened(state[i].view(-1))
+
+
 import gymnasium as gym
 import gymnasium_robotics
 import numpy as np
+import xenoworlds
+import matplotlib.pyplot as plt
 
-# Create the Ant environment
-env = gym.make(
-    "Ant-v2"
-)  # Replace with the specific Ant Maze environment if available
-# Reset the environment to get the initial state
-state = env.reset()
-# Hypothetically set the environment to a specific state
-# This requires understanding the environment's state structure
-desired_state = np.random.randn(10)  # Define your desired state here
-env.env.sim.set_state_from_flattened(desired_state)
-# Render the environment
-env.render()
-# Close the environment
-env.close()
+wrappers = [
+    # lambda x: RecordVideo(x, video_folder="./videos"),
+    lambda x: xenoworlds.wrappers.AddRenderObservation(x, render_only=False),
+    lambda x: xenoworlds.wrappers.TransformObservation(x),
+]
+# print(ogbench.make_env_and_datasets(env, env_only=True))
+world = xenoworlds.World(
+    "FrozenLake-v1", num_envs=2, wrappers=wrappers, max_episode_steps=2
+)
+
+world.envs.reset()
+img = world.envs.envs[0].render()
+plt.subplot(2, 4, 1)
+plt.imshow(img)
+plt.subplot(2, 4, 5)
+img = world.envs.envs[1].render()
+plt.imshow(img)
+
+xenoworlds.set_state(world, np.arange(2))
+img = world.envs.envs[0].render()
+plt.subplot(2, 4, 2)
+plt.imshow(img)
+plt.subplot(2, 4, 6)
+img = world.envs.envs[1].render()
+plt.imshow(img)
+
+
+world.envs.reset()
+img = world.envs.envs[0].render()
+plt.subplot(2, 4, 3)
+plt.imshow(img)
+plt.subplot(2, 4, 7)
+img = world.envs.envs[1].render()
+plt.imshow(img)
+
+xenoworlds.set_state(world, np.asarray([0, 3]))
+img = world.envs.envs[0].render()
+plt.subplot(2, 4, 4)
+plt.imshow(img)
+plt.subplot(2, 4, 8)
+img = world.envs.envs[1].render()
+plt.imshow(img)
+plt.savefig("test_delete.png")
+world.close()

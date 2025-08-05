121eabd9283d24b7aa6411c59cab6afc3454bc42
lucas-maes <lucas.maes7711@gmail.com>
2025-08-05 09:46:30 -0400

    add minimal example

--- xenoworlds/__init__.py
+++ xenoworlds/__init__.py
@@ -2,13 +2,17 @@ import os
 
 from . import planner
 from .world import World
-from .agent import Agent
 from .wm import DummyWorldModel
+
 from . import wrappers
 from .utils import set_state, create_pil_image_from_url
 from . import data
 from . import collect
 from gymnasium.envs.registration import register
+
+from .evaluator import Evaluator
+from .policy import BasePolicy, RandomPolicy, OptimalPolicy, PlanningPolicy
+from .solver import BaseSolver, GDSolver, CEMNevergrad, CEMSolver
 
 register(
     id="xenoworlds/ImagePositioning-v1",

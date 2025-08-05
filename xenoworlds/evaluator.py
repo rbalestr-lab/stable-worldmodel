35d0110d02298f476758c99046564dc8038fdafc
lucas-maes <lucas.maes7711@gmail.com>
2025-08-05 10:00:52 -0400

    boilerplate evaluator

--- xenoworlds/evaluator.py
+++ xenoworlds/evaluator.py
@@ -1,39 +1,44 @@
-from .solver import BaseSolver
+import torch
+from .policy import BasePolicy
 from .world import World
-
-### DataSetUpload (download using stable_ssl)
-
-# class Agent:
-#     def __init__(self, planner: BasePlanner, world: World):
-#         self.world = world
-#         self.planner = planner
-#         self.goals = self.planner.world_model.encode(
-#             torch.from_numpy(self.world.envs.reset()[0]["pixels"])
-#         )
-
-#     def run(self, episodes=1, max_steps=100):
-#         for episode in range(episodes):
-#             for states, rewards in self.world:
-#                 pixels = torch.from_numpy(states["pixels"])
-#                 actions = self.planner.plan(pixels, self.world.action_space, self.goals)
-#                 if actions.ndim == 2:
-#                     actions.unsqueeze_(0)
-#                 for a in actions.unbind(0):
-#                     self.world.step(a.numpy())
-#             print(f"Episode {episode + 1} finished ")
-#         self.world.close()
 
 
 ## -- Evaluator / Collector
 ### Evaluator(env, policy)
 class Evaluator:
     # the role of evaluator is to determine perf of the policy in the env
-    def __init__(self, world: World, solver: BaseSolver, goal_dist=None):
+    def __init__(self, world: World, policy: BasePolicy, goal_dist=None):
         self.world = world
-        self.solver = solver
-
+        self.policy = policy
         # sample goal
         self.goal_dist = goal_dist
 
     def run(self, episodes=1, max_steps=100):
-        pass
+        # todo return interested logging data
+        data = {}
+
+        for episode in range(episodes):
+            # sample goals and get their representations
+            goals = self.goal_dist.sample()
+            goals = self.solver.world_model.encode(goals).unsqueeze(0)
+
+            for states, rewards in self.world:
+                # get action from the policy wih optional goal specification
+                pixels = torch.from_numpy(states["pixels"])  # get the state
+                actions = self.policy.get_action(pixels, goals=goals)
+
+                # actions = actions.squeeze(0) if actions.ndim == 2 else actions
+                # apply actions in the env
+                # for a in actions.unbind(0):
+                #     self.world.step(a.numpy())
+
+                # todo assert action has the right shape
+                self.world.step(actions.numpy())
+
+            print(f"Episode {episode + 1} finished ")
+
+        self.world.close()
+        return data
+
+
+### DataSetUpload (download using stable_ssl)

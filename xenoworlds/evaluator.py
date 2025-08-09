ba65c03bc2e46e502c3a89ccbcb826d0c2d73e48
DanHrmti <danh93@gmail.com>
2025-08-09 18:27:09 -0400

    Initial Evaluator Supporting PushT + Random Policy
    Example Script:
    - remove observation wrappers amd return raw pixels from env
    - currently uses random policy
    - visualize rollouts and save video to logging dir
    
    Evaluator:
    - gets goal from world
    - saves frames for env visualization
    - prep for data logging
    
    World:
    - saves goal observation and goal image at reset
    
    PushT:
    - obs_mode argument to determine returned observation
    - action space change to [-1,1]
    - sample goal in reset
    - include [proprio, state, image] in info regardless of the obs_mode
    - FIX NEEDED: seems that the random seed for the parallel envs is the same for state sampling (see video visualization)

--- xenoworlds/evaluator.py
+++ xenoworlds/evaluator.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from .policy import BasePolicy
 from .world import World
@@ -7,38 +8,49 @@ from .world import World
 ### Evaluator(env, policy)
 class Evaluator:
     # the role of evaluator is to determine perf of the policy in the env
-    def __init__(self, world: World, policy: BasePolicy, goal_dist=None):
+    def __init__(self, world: World, policy: BasePolicy):
         self.world = world
         self.policy = policy
-        # sample goal
-        self.goal_dist = goal_dist
 
-    def run(self, episodes=1, max_steps=100):
-        # todo return interested logging data
-        data = {}
+    def run(self, episodes=1, video_episodes=1, max_steps=100):
+        # TODO return interested logging data
+        # NOTE Dan: we should have a convention for metrics to log and then get them from the infos
+        data = {
+            # "success": [],
+        }
+        frames_list = []
 
         for episode in range(episodes):
-            # sample goals and get their representations
-            goals = self.goal_dist.sample()
-            goals = self.solver.world_model.encode(goals).unsqueeze(0)
-
-            for states, rewards in self.world:
-                # get action from the policy wih optional goal specification
-                pixels = torch.from_numpy(states["pixels"])  # get the state
-                actions = self.policy.get_action(pixels, goals=goals)
-
+            frames = []
+            for states, infos in self.world:
+                # get action                
+                # NOTE Dan: It might be preferable that the environment return the raw pixel observation and the policy would take care of the transforms
+                obs = torch.from_numpy(states)
+                goal = torch.from_numpy(self.world.cur_goals)  # NOTE: would be more efficient if we get the goal once per episode
+                actions = self.policy.get_action(obs, goal)
                 # actions = actions.squeeze(0) if actions.ndim == 2 else actions
                 # apply actions in the env
                 # for a in actions.unbind(0):
                 #     self.world.step(a.numpy())
 
-                # todo assert action has the right shape
-                self.world.step(actions.numpy())
+                # save logging data
+                for key in data.keys():
+                    data[key].append(infos[key])
+                # save frames for video visualization
+                if episode < video_episodes:
+                    frames.append(np.concatenate([self.world.cur_goal_images[0], infos["image"][0]], axis=0))
 
+                # todo assert action has the right shape
+                self.world.step(actions)
+
+            if episode < video_episodes:
+                frames_list.append(np.array(frames))
+            
             print(f"Episode {episode + 1} finished ")
 
         self.world.close()
-        return data
+        data["frames_list"] = frames_list
 
+        return data
 
 ### DataSetUpload (download using stable_ssl)

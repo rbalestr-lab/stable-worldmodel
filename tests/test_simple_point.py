a8c921d2d37169902323a9fb9072e9b601955f5c
RandallBalestriero <rbalestriero@meta.com>
2025-09-10 02:22:05 +0000

    video recorder

--- tests/test_simple_point.py
+++ tests/test_simple_point.py
@@ -1,10 +1,5 @@
 def test_env():
-    import gymnasium as gym
-    import gymnasium_robotics
-    import torch
     import xenoworlds
-
-    # # run with MUJOCO_GL=egl python example.py
 
     world = xenoworlds.World(
         "xenoworlds/SimplePointMaze-v0",
@@ -12,7 +7,7 @@ def test_env():
         image_shape=(224, 224),
         render_mode="rgb_array",
     )
-    world.record_video("./video.mp4")
+    world.record_video("./")
     asdf
     world_model = xenoworlds.DummyWorldModel(image_shape=(3, 224, 224), action_dim=8)
 

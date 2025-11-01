8387d9f643deec9c15ef82b2b05584fb8bb863d4
lucas-maes <lucas.maes7711@gmail.com>
2025-11-01 17:36:40 -0400

    fix example

--- examples/example_pusht.py
+++ examples/example_pusht.py
@@ -1,4 +1,9 @@
 if __name__ == "__main__":
+    import datasets
+    import torch
+    from sklearn import preprocessing
+    from torchvision.transforms import v2 as transforms
+
     import stable_worldmodel as swm
 
     ######################
@@ -20,12 +25,12 @@ if __name__ == "__main__":
     # #######################
 
     world.set_policy(swm.policy.RandomPolicy())
-    # world.record_dataset(
-    #     "example-pusht",
-    #     episodes=10,
-    #     seed=2347,
-    #     options=None,
-    # )
+    world.record_dataset(
+        "example-pusht",
+        episodes=10,
+        seed=2347,
+        options=None,
+    )
 
     # world.record_video_from_dataset(
     #     "./",
@@ -37,59 +42,59 @@ if __name__ == "__main__":
     ##  Pretrain  ##
     ################
 
-    # swm.pretraining(
-    #     "scripts/train/dinowm.py",
-    #     dataset_name="example-pusht",
-    #     output_model_name="dummy_pusht",
-    #     dump_object=True,
-    # )
+    swm.pretraining(
+        "scripts/train/dinowm.py",
+        dataset_name="example-pusht",
+        output_model_name="dummy_pusht",
+        dump_object=True,
+    )
 
     #########################
     ##  Transform/Process  ##
     #########################
 
-    # def img_transform():
-    #     transform = transforms.Compose(
-    #         [
-    #             transforms.Resize(size=224),
-    #             transforms.CenterCrop(size=224),
-    #             transforms.ToImage(),
-    #             transforms.ToDtype(torch.float32, scale=True),
-    #             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    #         ]
-    #     )
-    #     return transform
+    def img_transform():
+        transform = transforms.Compose(
+            [
+                transforms.Resize(size=224),
+                transforms.CenterCrop(size=224),
+                transforms.ToImage(),
+                transforms.ToDtype(torch.float32, scale=True),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
+        return transform
 
-    # transform = {
-    #     "pixels": img_transform(),
-    #     "goal": img_transform(),
-    # }
+    transform = {
+        "pixels": img_transform(),
+        "goal": img_transform(),
+    }
 
-    # dataset_path = swm.data.get_cache_dir() / "example-pusht"
-    # dataset = datasets.load_from_disk(dataset_path).with_format("numpy")
+    dataset_path = swm.data.get_cache_dir() / "example-pusht"
+    dataset = datasets.load_from_disk(dataset_path).with_format("numpy")
 
-    # action_process = preprocessing.StandardScaler()
-    # action_process.fit(dataset["action"][:])
+    action_process = preprocessing.StandardScaler()
+    action_process.fit(dataset["action"][:])
 
-    # proprio_process = preprocessing.StandardScaler()
-    # proprio_process.fit(dataset["proprio"][:])
+    proprio_process = preprocessing.StandardScaler()
+    proprio_process.fit(dataset["proprio"][:])
 
-    # process = {
-    #     "action": action_process,
-    #     "proprio": proprio_process,
-    #     "goal_proprio": proprio_process,
-    # }
+    process = {
+        "action": action_process,
+        "proprio": proprio_process,
+        "goal_proprio": proprio_process,
+    }
 
     ################
     ##  Evaluate  ##
     ################
 
-    # model = swm.policy.AutoCostModel("dummy_pusht").to("cuda")
-    # config = swm.PlanConfig(horizon=5, receding_horizon=5, action_block=5)
-    # solver = swm.solver.CEMSolver(model, num_samples=300, var_scale=1.0, n_steps=30, topk=30, device="cuda")
-    # policy = swm.policy.WorldModelPolicy(solver=solver, config=config, process=process, transform=transform)
+    model = swm.policy.AutoCostModel("dummy_pusht").to("cuda")
+    config = swm.PlanConfig(horizon=5, receding_horizon=5, action_block=5)
+    solver = swm.solver.CEMSolver(model, num_samples=300, var_scale=1.0, n_steps=30, topk=30, device="cuda")
+    policy = swm.policy.WorldModelPolicy(solver=solver, config=config, process=process, transform=transform)
 
-    # world.set_policy(policy)
+    world.set_policy(policy)
 
     results = world.evaluate(episodes=20, seed=42, dump_every=10)
 

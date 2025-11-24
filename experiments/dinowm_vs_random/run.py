2ae7121a4ac148ff3d4c786eaec839de82e231ff
quentinll <quentin.lelidec@protonmail.com>
2025-11-23 21:25:13 -0500

    Update evaluation configuration and add DinoV2 encoder to run.py

--- experiments/dinowm_vs_random/run.py
+++ experiments/dinowm_vs_random/run.py
@@ -85,6 +85,31 @@ def run(cfg: DictConfig):
         config = swm.PlanConfig(**cfg.plan_config)
         solver = hydra.utils.instantiate(cfg.solver, model=model)
         policy = swm.policy.WorldModelPolicy(solver=solver, config=config, process=process, transform=transform)
+        torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
+
+        class DinoV2Encoder(torch.nn.Module):
+            def __init__(self, name, feature_key):
+                super().__init__()
+                self.name = name
+                self.base_model = torch.hub.load("facebookresearch/dinov2", name)
+                self.feature_key = feature_key
+                self.emb_dim = self.base_model.num_features
+                if feature_key == "x_norm_patchtokens":
+                    self.latent_ndim = 2
+                elif feature_key == "x_norm_clstoken":
+                    self.latent_ndim = 1
+                else:
+                    raise ValueError(f"Invalid feature key: {feature_key}")
+
+                self.patch_size = self.base_model.patch_size
+
+            def forward(self, x):
+                emb = self.base_model.forward_features(x)[self.feature_key]
+                if self.latent_ndim == 1:
+                    emb = emb.unsqueeze(1)  # dummy patch dim
+                return emb
+
+        model.backbone = DinoV2Encoder("dinov2_vits14", feature_key="x_norm_patchtokens").to("cuda")
 
     # sample the episodes and the starting indices
     episode_len = get_episodes_length(dataset, ep_indices)

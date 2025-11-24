b91bfee3b6c1aa9d81624b97e67c0117d5056372
lucas-maes <lucas.maes7711@gmail.com>
2025-11-24 13:55:06 -0500

    patch main

--- stable_worldmodel/wm/__init__.py
+++ stable_worldmodel/wm/__init__.py
@@ -1,13 +1,10 @@
-from . import dinowm, pyro
+from . import dinowm
 from .dinowm import DINOWM
 from .dummy import DummyWorldModel  # noqa: F401
-from .pyro import PYRO
 
 
 __all__ = [
     "DummyWorldModel",
     "DINOWM",
     "dinowm",
-    "pyro",
-    "PYRO",
 ]

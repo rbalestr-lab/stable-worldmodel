2039279b46aedc185452a1964fcb00b34aa059f5
lucas-maes <lucas.maes7711@gmail.com>
2025-11-29 17:27:28 -0500

    expert policy tworoom

--- stable_worldmodel/envs/simple_nav/__init__.py
+++ stable_worldmodel/envs/simple_nav/__init__.py
@@ -1,5 +1,5 @@
+from .env import SimpleNavigationEnv
 from .expert_policy import ExpertPolicy
-from .simple_navigation import SimpleNavigationEnv
 
 
 __all__ = ["ExpertPolicy", "SimpleNavigationEnv"]

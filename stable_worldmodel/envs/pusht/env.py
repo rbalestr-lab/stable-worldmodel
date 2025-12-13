80e9d14951d3dc56a0f1d6b39584ff9b80bf8c49
quentinll <quentin.lelidec@protonmail.com>
2025-12-13 10:00:34 -0500

    Add option to set goal state in PushT reset method

--- stable_worldmodel/envs/pusht/env.py
+++ stable_worldmodel/envs/pusht/env.py
@@ -234,14 +234,17 @@ class PushT(gym.Env):
             self.space.damping = self.damping
 
         ### get the state
-        goal_state = np.concatenate(
-            [
-                self.variation_space["agent"]["start_position"].sample(set_value=False).tolist(),
-                self.variation_space["block"]["start_position"].sample(set_value=False).tolist(),
-                [self.variation_space["block"]["angle"].sample(set_value=False)],
-                self.variation_space["agent"]["velocity"].value.tolist(),
-            ]
-        )
+        if options is not None and "goal_state" in options:
+            goal_state = options["goal_state"]
+        else:
+            goal_state = np.concatenate(
+                [
+                    self.variation_space["agent"]["start_position"].sample(set_value=False).tolist(),
+                    self.variation_space["block"]["start_position"].sample(set_value=False).tolist(),
+                    [self.variation_space["block"]["angle"].sample(set_value=False)],
+                    self.variation_space["agent"]["velocity"].value.tolist(),
+                ]
+            )
 
         ### generate goal
         self._set_state(goal_state)

dc1f4ade0909304c208f4ca9cf4897accd7975f3
quentinll <quentin.lelidec@protonmail.com>
2025-12-22 18:08:57 -0500

    fixing eval script to use pyro with dinov2 with 20 epochs

--- experiments/dinowm_vs_random/run.py
+++ experiments/dinowm_vs_random/run.py
@@ -84,36 +84,6 @@ def run(cfg: DictConfig):
     policy = swm.policy.RandomPolicy(cfg.seed)
     if cfg.policy != "random":
         model = swm.policy.AutoCostModel(cfg.policy).to("cuda")
-        torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
-
-        class DinoV2Encoder(torch.nn.Module):
-            def __init__(self, name, feature_key):
-                super().__init__()
-                self.name = name
-                self.base_model = torch.hub.load("facebookresearch/dinov2", name)
-                self.feature_key = feature_key
-                self.emb_dim = self.base_model.num_features
-                if feature_key == "x_norm_patchtokens":
-                    self.latent_ndim = 2
-                elif feature_key == "x_norm_clstoken":
-                    self.latent_ndim = 1
-                else:
-                    raise ValueError(f"Invalid feature key: {feature_key}")
-
-                self.patch_size = self.base_model.patch_size
-
-            def forward(self, x):
-                emb = self.base_model.forward_features(x)[self.feature_key]
-                if self.latent_ndim == 1:
-                    emb = emb.unsqueeze(1)  # dummy patch dim
-                return emb
-
-        ckpt = torch.load(swm.data.utils.get_cache_dir() / "dinowm_pusht_weights.ckpt")
-        model.backbone = DinoV2Encoder("dinov2_vits14", feature_key="x_norm_patchtokens").to("cuda")
-        model.predictor.load_state_dict(ckpt["predictor"], strict=False)
-        model.action_encoder.load_state_dict(ckpt["action_encoder"])
-        model.proprio_encoder.load_state_dict(ckpt["proprio_encoder"])
-        model = model.to("cuda")
         model = model.eval()
 
         config = swm.PlanConfig(**cfg.plan_config)

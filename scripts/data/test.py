183f0d2b570b9175562459f097a0a7749c77fd93
quentinll <quentin.lelidec@protonmail.com>
2026-01-21 23:47:51 -0500

    Remove AcrobotDMControl wrapper from test script

--- scripts/data/test.py
+++ scripts/data/test.py
@@ -6,16 +6,6 @@ os.environ["MUJOCO_GL"] = "egl"
 import stable_worldmodel as swm
 
 
-world = swm.World(env_name="swm/AcrobotDMControl-v0", image_shape=(224, 224), num_envs=1, max_episode_steps=1000)
-world.set_policy(swm.policy.RandomPolicy())
-print("acrobot")
-for i in range(10):
-    world.record_video(
-        "./",
-        seed=i,
-        options={"variation": ("all",)},
-    )
-
 world = swm.World(env_name="swm/CartpoleDMControl-v0", image_shape=(224, 224), num_envs=1, max_episode_steps=1000)
 world.set_policy(swm.policy.RandomPolicy())
 print("cartpole")
